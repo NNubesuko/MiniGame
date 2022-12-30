08a4c4fd2008b7ca1d6c518cc550c474e5f3f9a1
YoshinobuKataoka <yoshi37770129@au.com>
2022-12-30 17:21:22 +0900

    剣の生成

--- Assets/RectRange.cs
+++ Assets/RectRange.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RectRange {
@@ -9,6 +10,8 @@ public class RectRange {
     public Vector2 start => new Vector2(startX, startY);
     public Vector2 end => new Vector2(endX, endY);
 
+    private List<Vector3> list = new List<Vector3>();
+
     public RectRange(
         float startX,
         float startY,
@@ -19,17 +22,32 @@ public class RectRange {
         this.startY = startY;
         this.endX = endX;
         this.endY = endY;
+
+        InitList(startX, startY, endX, endY);
     }
 
-    public RectRange(Vector2 start, Vector2 end) {
-        this.startX = start.x;
-        this.startY = start.y;
-        this.endX = end.x;
-        this.endY = end.y;
+    public RectRange(Vector2 start, Vector2 end) : this(start.x, start.y, end.x, end.y) {
     }
 
     public override string ToString() {
         return $"({startX}, {startY}) ~ ({endX}, {endY})";
+    }
+
+    private void InitList(float startX, float startY, float endX, float endY) {
+        for (float x = startX; x <= endX; x+=1) {
+            for (float z = startY; z <= endY; z+=1) {
+                list.Add(new Vector3(x, 0f, z));
+            }
+        }
+    }
+
+    public (Vector3 position, bool exist) RandomPosition() {
+        if (list.Count == 0) return (default, false);
+
+        Vector3 position = list[Random.Range(0, list.Count)];
+        list.Remove(position);
+
+        return (position, true);
     }
 
 }
--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,7 +4,38 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
 
+    [SerializeField] private GameObject[] axe;
+    [SerializeField] private GameObject[] hammer;
+    [SerializeField] private GameObject[] sword;
+    [SerializeField] private GameObject generateWeaponsRangeStartPoint;
+    [SerializeField] private GameObject generateWeaponsRangeEndPoint;
+
+    private RectRange generateWeaponsRange;
+    private Vector3[] weaponsPositionArray;
+
+    private List<Vector3> weaponsPositionList = new List<Vector3>();
+
     private void Awake() {
+        // 範囲の開始地点と終了地点から座標を取得
+        Vector3 generateWeaponsRangeStart = generateWeaponsRangeStartPoint.transform.position;
+        Vector3 generateWeaponsRangeEnd = generateWeaponsRangeEndPoint.transform.position;
+
+        // 四角形の範囲クラスを初期化
+        generateWeaponsRange = new RectRange(
+            new Vector2(generateWeaponsRangeStart.x, generateWeaponsRangeStart.z),
+            new Vector2(generateWeaponsRangeEnd.x, generateWeaponsRangeEnd.z)
+        );
+
+        while (true) {
+            (Vector3 position, bool exist) = generateWeaponsRange.RandomPosition();
+            if (!exist) break;
+
+            Instantiate(
+                sword[Random.Range(0, sword.Length)],
+                position + Vector3.up,
+                Quaternion.Euler(90f, 0f, 0f)
+            );
+        }
     }
 
     private void Update() {
