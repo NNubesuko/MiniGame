4ddfbbe57b078113517e2e8fc847e250b7694263
YoshinobuKataoka <yoshi37770129@au.com>
2022-12-30 22:52:36 +0900

    武器を指定した本数生成されるようにした

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
 
+    [Header("各武器のゲームオブジェクト")]
     [SerializeField] private GameObject[] axe;
     [SerializeField] private GameObject[] hammer;
     [SerializeField] private GameObject[] sword;
+
+    [Header("各武器の生成数")]
+    [SerializeField] private float numberAxe;
+    [SerializeField] private float numberHammer;
+    [SerializeField] private float numberSword;
+
     [SerializeField] private GameObject generateWeaponsRangeStartPoint;
     [SerializeField] private GameObject generateWeaponsRangeEndPoint;
 
@@ -26,19 +33,33 @@ public class Test : MonoBehaviour {
             new Vector2(generateWeaponsRangeEnd.x, generateWeaponsRangeEnd.z)
         );
 
-        while (true) {
-            (Vector3 position, bool exist) = generateWeaponsRange.RandomPosition();
-            if (!exist) break;
+        GenerateWeapons(numberAxe, axe);
+        GenerateWeapons(numberHammer, hammer);
+        GenerateWeapons(numberSword, sword);
 
-            Instantiate(
-                sword[Random.Range(0, sword.Length)],
-                position + Vector3.up,
-                Quaternion.Euler(90f, 0f, 0f)
-            );
-        }
+        // while (true) {
+        //     (Vector3 position, bool exist) = generateWeaponsRange.RandomPosition();
+        //     if (!exist) break;
+
+        //     Instantiate(
+        //         sword[Random.Range(0, sword.Length)],
+        //         position + Vector3.up,
+        //         Quaternion.Euler(90f, 0f, 0f)
+        //     );
+        // }
     }
 
     private void Update() {
+    }
+
+    private void GenerateWeapons(float number, GameObject[] gameObjects) {
+        for (int i = 0; i < number; i++) {
+            (Vector3 position, bool exist) = generateWeaponsRange.RandomPosition();
+            if (!exist) break;
+
+            GameObject gameObject = gameObjects[Random.Range(0, gameObjects.Length)];
+            Instantiate(gameObject, position + Vector3.up, Quaternion.Euler(90f, 0f, 0f));
+        }
     }
 
 }
